c06fdbb956675563e006edcb37fd2bf672b39b2f
robert <>
2020-07-20 08:11:44 +0100

    Add quoted text version

--- runcmd.sh
+++ runcmd.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ################################################################################
 # Simple routine to embed a nice scriptfile into a RUN command.
@@ -11,9 +11,22 @@ make_docker_runcmd() {
     echo ")|base64 -d|gzip -d>$sname;sh -ex $sname;rm -f $sname"
 }
 
+make_docker_runtxt() {
+    sn="/tmp/install"
+    echo "RUN (\\"
+    cat "$@" |
+	while IFS= read -r line
+	do echo echo "${line@Q};"\\
+	done
+    echo ")>$sn;sh -e $sn;rm -f $sn"
+    return 0;
+}
+
 main() {
     if [ "$1" = -u ]
     then shift ; cat "$@" | grep ^_ | base64 -di | gzip -d
+    elif [ "$1" = -x ]
+    then shift ; make_docker_runtxt "$@"
     else make_docker_runcmd "$@"
     fi
 }
