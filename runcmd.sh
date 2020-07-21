caf7ce249fa1c83a5c6f4b48d62e5d3e096c5ff2
robert <>
2020-07-21 20:20:58 +0100

    Downgrade the -x variant to an also-ran.

--- runcmd.sh
+++ runcmd.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+main() {
+    case "$1" in
+    '' ) make_docker_runcmd "$0" '' start ;;
+    start) guest_start; exit;;
+
+    -u ) shift ; cat "$@" | grep ^_ | base64 -di | gzip -d ;;
+
+    -x ) shift
+	sn="/tmp/install"
+	echo "RUN (\\"
+	cat "$@" |
+	    while IFS= read -r line
+	    do echo echo "${line@Q};\\"
+	    done
+	echo ")>$sn;sh -e $sn;rm -f $sn"
+	exit
+	;;
+
+    * ) make_docker_runcmd "$@" ;;
+    esac
+}
+
 ################################################################################
 # Simple routine to embed a nice scriptfile into a RUN command.
 
@@ -8,27 +30,7 @@ make_docker_runcmd() {
     script="$1" ; sname="${2:-/tmp/install}"
     echo 'RUN set -eu;_() { echo "$@";};'"(\\"
     gzip -cn9 "$script" | base64 -w 72 | sed 's/.*/_ &;\\/'
-    echo ")|base64 -d|gzip -d>$sname;sh -ex $sname;rm -f $sname"
-}
-
-make_docker_runtxt() {
-    sn="/tmp/install"
-    echo "RUN (\\"
-    cat "$@" |
-	while IFS= read -r line
-	do echo echo "${line@Q};"\\
-	done
-    echo ")>$sn;sh -e $sn;rm -f $sn"
-    return 0;
-}
-
-main() {
-    if [ "$1" = -u ]
-    then shift ; cat "$@" | grep ^_ | base64 -di | gzip -d
-    elif [ "$1" = -x ]
-    then shift ; make_docker_runtxt "$@"
-    else make_docker_runcmd "$@"
-    fi
+    echo ")|base64 -d|gzip -d>$sname;sh -e $sname${3:+ $3};rm -f $sname"
 }
 
 main "$@"
