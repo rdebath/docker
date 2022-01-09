62d9b0636eae96a561d963d3c8411fbcf19bc315
Robert de Bath <rdebath@tvisiontech.co.uk>
2022-01-09 10:50:30 +0000

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -
 true , /^; exec awk -f "$0" -- "$@" ; exit #/ {}
 BEGIN { # vim: set filetype=awk:
     sh = "sh"; txtmode = 0;
@@ -40,16 +40,17 @@ BEGIN { # vim: set filetype=awk:
     mode=0; ln=""; ty="";
 }
 
-# Embedded docker file in dockerfile() function.
+# Embedded docker file in dockerfile() function, or append after "DOCKERFILE" line
 /^#!\/bin\/[a-z]*sh\>/ && mode==0 { mode=3; ln="encode<<\\@\n"; }
 mode==3 &&/^dockerfile\(\) *{ *$/ { print dsed|sh; mode=4;next;}
 mode==4 &&/^RUN *$/ { mode=5; next;}
 mode>=4 &&/^} *$/ { print "@"|sh; mode=3; next;}
+/^DOCKERFILE/ && mode==3 { mode=0; ln=""; next; }
 mode==3 { if (substr($0, 1, 1) == "@") ln=ln"@"; ln=ln $0 "\n"; next; }
 mode==5 { if (substr($0, 1, 1) == "@") ty=ty"@"; ty=ty $0 "\n"; next; }
 END { if (mode>=4) ln="@\n"ln;}
 
-# Standard with BEGIN and COMMIT translation
+# Standard Dockerfile with BEGIN and COMMIT translation
 /^BEGIN$/||/^BEGIN /{ if (mode) print "@"|sh; mode=2; $1="encode<<\\@"; print|sh; next }
 /^COMMIT *$/ && mode==2 { print "@"|sh; mode=0; next; }
 mode==0 { print dsed|sh; mode=1; }
